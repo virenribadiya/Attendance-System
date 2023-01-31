145be0ecbb9d8a4f483f5a2850bf3da5410e257d
virenribadiya <vribadiya278@rku.ac.in>
2023-01-31 11:29:53 +0530

    NavBar component created

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light my-navbar">
+        <div class="container">
+            <router-link class="navbar-brand" :to="{name:'home'}"> 
+               <div class="text-centre">Attandance System</div> 
+            </router-link>
+        </div>
+    </nav>
+</template>
+
+
+
+
+<script>
+export default {
+    name: "NavbarComponent",
+    data(){
+        return{
+
+        }
+    }
+}
+
+</script>
+
+
+
+
+<style>
+.my-navbar{
+    border-bottom: 1px solid #dddddd;
+}
+
+.navbar-brand{
+    font-weight:bold;
+    font-size: 150%
+}
+.navbar-brand:hover{
+    color: #736c6c !important;
+}
+</style>
