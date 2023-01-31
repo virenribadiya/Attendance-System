eb93afb800ef7a6e94b9564d0ccccd5da26a2dbf
virenribadiya <vribadiya278@rku.ac.in>
2023-01-31 11:29:12 +0530

    Vue Project Created

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavbarComponent /> 
+  <router-view/>
+</template>
+
+<script >
+import NavbarComponent from "@/components/Navbar.vue";
+
+export default{
+  name:"App",
+  components: {
+    NavbarComponent,
+}
+}
+</script>
